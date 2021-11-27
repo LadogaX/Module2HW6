0685d2a71c2485241721f7e514c3a908b8559ae2
LadogaX <ladogax@gmail.com>
2021-11-27 14:34:53 +0200

    Empty project

---
+++ Module2HW6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module2HW6
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
